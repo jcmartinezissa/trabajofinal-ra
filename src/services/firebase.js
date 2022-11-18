8912be72209ef3f1ad37704cf24c3c4f30e4ecff
Juan Carlos Martinez Issa <carloswolvi@gmail.com>
2022-11-17 23:25:24 -0300

    add connection firebase

---
+++ src/services/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBRWN1TcrVw5YBcfAzy3sS79Z5JOalQ_Qk',
+  authDomain: 'rollingcodeapp2022.firebaseapp.com',
+  projectId: 'rollingcodeapp2022',
+  storageBucket: 'rollingcodeapp2022.appspot.com',
+  messagingSenderId: '377365448322',
+  appId: '1:377365448322:web:53ffb57906bb290428169b',
+};
+
+const app = initializeApp(firebaseConfig);
+export default app;
