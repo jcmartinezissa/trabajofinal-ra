110756dc385418e1c7484d08f30bfc5704d3b71a
Juan Carlos Martinez Issa <carloswolvi@gmail.com>
2022-11-18 00:06:33 -0300

    add getAuth, getFirestore

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,4 +1,6 @@
 import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyBRWN1TcrVw5YBcfAzy3sS79Z5JOalQ_Qk',
@@ -10,4 +12,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export default app;
+export const { auth } = getAuth(app);
+export const db = getFirestore(app);
+
+export default { app, db };
