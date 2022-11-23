122e0bc1ccc33f6e3d6282ea74c7859ae7f3b043
marcosarias21 <marcosariasolaiz@gmail.com>
2022-11-23 09:33:37 -0300

    estructura de beneficios creadas , ademas hice un componente que pasa data al home en vez de poner el codigo directamente en el home

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Beneficios } from './src/screens/Beneficios';
 import { Home } from './src/screens/Home';
 
 const Stack = createNativeStackNavigator();
@@ -9,6 +10,7 @@ const App = () => {
       <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Beneficios" component={Beneficios} />
         </Stack.Navigator>
       </NavigationContainer>
   );
--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,16 +1,8 @@
-import { View, Text, TouchableOpacity } from 'react-native-web';
-import { styles } from '../../../appStyles';
-import { sections } from '../../helper/sections';
+import { Sections } from '../../components/Sections';
 
 const Home = () => {
   return (
-    <View style={styles.container}>
-      {sections.map(section => (
-      <TouchableOpacity style={styles.button} key={section.title}>
-        <Text style={{ fontSize: 20 }}> {section.title} </Text>
-      </TouchableOpacity>
-      ))}
-   </View>
+    <Sections />
   );
 };
 
