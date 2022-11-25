942bd8c8bd2f8a5b9a8991716862e015bd8684ca
Norali Meriles <merilesnorali.1996@gmail.com>
2022-11-25 20:43:58 -0300

    login funcionando2.0

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { View, Text } from 'react-native-web';
+import { View, Text } from 'react-native';
 import { styles } from '../../../appStyles';
 
 const Home = () => {
