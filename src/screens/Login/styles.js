a7ef543c6eec087e2b99c57748d44861fbaaa72a
Norali Meriles <merilesnorali.1996@gmail.com>
2022-12-13 07:23:48 -0300

    ajustes

--- src/screens/Login/styles.js
+++ src/screens/Login/styles.js
@@ -3,7 +3,7 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
   titleLogin: {
     margin: 'auto',
-    fontWeight: 600,
+    // fontWeight: 600,
     marginBottom: 12,
   },
   input: {
